5a70a8fbd426c090344645bf822c4d9684c01048
Simon Norberg <simon@dackbrann.net>
2010-08-01 17:24:22 +0200

    plays internet-radio on wakeup
    instead of local files

--- wakeUpAction.py
+++ wakeUpAction.py
@@ -1,20 +1,23 @@
-import time, os
+import time
+from subprocess import Popen
 import writeLED
 
 #add actions to be done on wakeup here
 def onWakeUp():
 	#toke to much cpu :(
 	#os.popen("play ~/music/*.wav fade h 60 &> /dev/null")
-	for i in xrange(255):
-		writeLED.writeLED_PWM("w", i)
-		time.sleep(0.4)
-	os.popen("alsaplayer ~/music/*.au &> /dev/null")
+	Popen(["mplayer", "http://http-live.sr.se/p3-mp3-192"])
+	print "next"
+#	for i in xrange(255):
+#		#writeLED.writeLED_PWM("w", i)
+#		time.sleep(0.4)
+#	os.popen("alsaplayer ~/music/*.au &> /dev/null")
 
 #cleanup action to do after wakeup
 def afterWakeUp():
-	#turn of musix
-	os.popen("killall alsaplayer")
-	writeLED.writeLED_PWM("w", 0)
+	#turn of musixc
+	Popen(["killall", "mplayer"])
+	#writeLED.writeLED_PWM("w", 0)
 
 if __name__ == "__main__":
 	onWakeUp()
